486876635e33ad4ddf8f4fd5a6ad607f799a44c6
danknjoroge <dank8505@gmail.com>
2022-01-18 22:28:16 +0300

    added validation function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 
-
-          
+function validateForm() {
+  var name= document.getElementById("name").value;
+  var phone= document.getElementById("phone").value;
+  if( name== ""){
+      alert("Name required");
+  }else if( phone ==""){
+    alert("Phone required");
+    
+  }
+  else{
+    return true;
+  }
+}
+//function for image upload
+function showPreview(event){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = document.getElementById("file-ip-1-preview");
+      preview.src = src;
+      preview.style.display = "block";
+    }
+  }
