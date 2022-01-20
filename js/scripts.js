bef6b1efe59da0eb9ada1717b9ebc0d0f3501c70
Soila Pertet <nicolepertet@gmail.com>
2022-01-20 04:51:21 +0300

    Create a function which calculates and returns the cumultative GPA for all semesters

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@
 
 // Define/Initiate variables
 var courses = []; 
-var totalWeightedGPA, totalCredits,semesterGPA,cumultativeGPA;
+var courseGradePoints= [];
+var credits = [];
+var totalWeightedGPA, totalCredits,semesterGPA,cumultativeGPA,gradePointsEarned,creditsEarned;
 
 // Create a 'Course' constructor function
 function Course(course,credits,letterGrade){
@@ -21,13 +23,47 @@ function calculateSemesterGPA(){
   var totalCredits = 0;
   
   $(courses).each(function(){
-    totalWeightedGPA += this.courseCredits * this.courseGrade;
+    totalWeightedGPA += (this.courseCredits * this.courseGrade) * 1 ;
     totalCredits += (this.courseCredits * 1);
 
-    var semesterGPA = (totalWeightedGPA / totalCredits).toFixed(2);
+    var semesterGPA = parseFloat((totalWeightedGPA / totalCredits).toFixed(2));
     console.log(semesterGPA);
+
   });
-  return semesterGPA;
+
+  courseGradePoints.push(totalWeightedGPA);
+  console.log(courseGradePoints);
+  credits.push(totalCredits);
+  console.log(credits);
+  // return semesterGPA;
+
+}
+
+// Create a function to calculate Cumultative GPA
+function calculateCumultativeGPA(){
+
+  var gradePointsEarned = 0;
+
+  for(i=0;i < courseGradePoints.length;i++) {
+    gradePointsEarned += courseGradePoints[i];
+    console.log(gradePointsEarned);
+  }
+
+  var creditsEarned = 0;
+
+  for(i=0;i < credits.length;i++) {
+    creditsEarned += credits[i];
+    console.log(creditsEarned);
+  }
+
+  var cumultativeGPA = parseFloat((gradePointsEarned / creditsEarned).toFixed(2));
+  console.log(cumultativeGPA);
+
+  var inputtedSemesterName = $("input#semester-name").val();
+
+  var row = "<tr><th>"+inputtedSemesterName+"</th><th>"+cumultativeGPA+"</th></tr>";
+    $("tbody#cumultative-gpa").append(row);
+  // return cumultativeGPA;
 }
 
 $(document).ready(function(){
@@ -37,203 +73,76 @@ $(document).ready(function(){
   })
   // Add semester section after clicking 'Add semester' button
   $("#add-semester").click(function(){
-    $(".new-semester").append(
-      '<br>'+
-      '<div class="new-semester">'+
-        '<div class="card">'+
-          '<div class="card-header">'+
-            '<div class="row">'+
-              '<div class="col-md-12">'+
-                '<h2><input class="border" type="text" placeholder="Semester e.g Fall 2021"></h2>'+
-              '</div>'+
-            '</div>' +
-          '</div>'+
-          '<div class="card-body">'+
-            '<form action="calculate-gpa">'+
-              '<div id="course-grades">'+
-                '<div class="course" class="form-group">'+
-                  '<div class="row">'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Name" id="course-name" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<select name="letter-grade" id="letter-grade" class="form-select" required>'+
-                        '<option value="" hidden required>Letter Grade</option>'+
-                        '<option value="4.0">A+</option>'+
-                        '<option value="4.0">A</option>'+
-                        '<option value="3.7">A-</option>'+
-                        '<option value="3.3">B+</option>'+
-                        '<option value="3.0">B-</option>'+
-                        '<option value="2.7">B-</option>'+
-                        '<option value="2.3">C+</option>'+
-                        '<option value="2.0">C</option>'+
-                        '<option value="1.7">C-</option>'+
-                        '<option value="1.3">D+</option>'+
-                        '<option value="1.0">D</option>'+
-                        '<option value="0.0">F</option>'+
-                     ' </select>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<button type="button" class="remove" id="remove-course" aria-label="Remove Course">'+
-                      '</button>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-                '<br>'+
-                '<div class="course" class="form-group">'+
-                  '<div class="row">'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Name" id="course-name" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<select name="letter-grade" id="letter-grade" class="form-select" required>'+
-                        '<option value="" hidden required>Letter Grade</option>'+
-                        '<option value="4.0">A+</option>'+
-                        '<option value="4.0">A</option>'+
-                        '<option value="3.7">A-</option>'+
-                        '<option value="3.3">B+</option>'+
-                        '<option value="3.0">B-</option>'+
-                        '<option value="2.7">B-</option>'+
-                        '<option value="2.3">C+</option>'+
-                        '<option value="2.0">C</option>'+
-                        '<option value="1.7">C-</option>'+
-                        '<option value="1.3">D+</option>'+
-                        '<option value="1.0">D</option>'+
-                        '<option value="0.0">F</option>'+
-                     ' </select>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<button type="button" class="remove" id="remove-course" aria-label="Remove Course">'+
-                      '</button>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-                '<br>'+
-                '<div class="course" class="form-group">'+
-                  '<div class="row">'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Name" id="course-name" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<select name="letter-grade" id="letter-grade" class="form-select" required>'+
-                        '<option value="" hidden required>Letter Grade</option>'+
-                        '<option value="4.0">A+</option>'+
-                        '<option value="4.0">A</option>'+
-                        '<option value="3.7">A-</option>'+
-                        '<option value="3.3">B+</option>'+
-                        '<option value="3.0">B-</option>'+
-                        '<option value="2.7">B-</option>'+
-                        '<option value="2.3">C+</option>'+
-                        '<option value="2.0">C</option>'+
-                        '<option value="1.7">C-</option>'+
-                        '<option value="1.3">D+</option>'+
-                        '<option value="1.0">D</option>'+
-                        '<option value="0.0">F</option>'+
-                     ' </select>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<button type="button" class="remove" id="remove-course" aria-label="Remove Course">'+
-                      '</button>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-                '<br>'+
-                '<div class="course" class="form-group">'+
-                  '<div class="row">'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Name" id="course-name" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<select name="letter-grade" id="letter-grade" class="form-select" required>'+
-                        '<option value="" hidden required>Letter Grade</option>'+
-                        '<option value="4.0">A+</option>'+
-                        '<option value="4.0">A</option>'+
-                        '<option value="3.7">A-</option>'+
-                        '<option value="3.3">B+</option>'+
-                        '<option value="3.0">B-</option>'+
-                        '<option value="2.7">B-</option>'+
-                        '<option value="2.3">C+</option>'+
-                        '<option value="2.0">C</option>'+
-                        '<option value="1.7">C-</option>'+
-                        '<option value="1.3">D+</option>'+
-                        '<option value="1.0">D</option>'+
-                        '<option value="0.0">F</option>'+
-                     ' </select>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<button type="button" class="remove" id="remove-course" aria-label="Remove Course">'+
-                      '</button>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-                '<br>'+
-                '<div class="course" class="form-group">'+
-                  '<div class="row">'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Name" id="course-name" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                      '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control" required>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<select name="letter-grade" id="letter-grade" class="form-select" required>'+
-                        '<option value="" hidden required>Letter Grade</option>'+
-                        '<option value="4.0">A+</option>'+
-                        '<option value="4.0">A</option>'+
-                        '<option value="3.7">A-</option>'+
-                        '<option value="3.3">B+</option>'+
-                        '<option value="3.0">B-</option>'+
-                        '<option value="2.7">B-</option>'+
-                        '<option value="2.3">C+</option>'+
-                        '<option value="2.0">C</option>'+
-                        '<option value="1.7">C-</option>'+
-                        '<option value="1.3">D+</option>'+
-                        '<option value="1.0">D</option>'+
-                        '<option value="0.0">F</option>'+
-                     ' </select>'+
-                    '</div>'+
-                    '<div class="col-md-2">'+
-                      '<button type="button" class="remove" id="remove-course" aria-label="Remove Course">'+
-                      '</button>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-              '</div>'+
-              '<br>'+
-              '<button class="button-17" id="calculate">Calculate GPA</button>'+
-            '</form>'+
-         '</div>'+
-        '</div>'+
-      '</div>'      
-    );
+  
     $(".remove").click(function(){
       $(this).closest(".course").hide();
     });
-  });
-  $("#calculate").click(function(){
-    event.preventDefault();
-    $(".course").each(function(){
-      var inputtedCourseName = $(this).find("input#course-name").val();
-      var inputtedCourseCredit = parseFloat($(this).find("input#course-credits").val());
-      var selectedGradePoint = parseFloat($(this).find("select option:selected").val());
-      var inputtedCourse = new Course(inputtedCourseName,inputtedCourseCredit,selectedGradePoint);
-    
-      courses.push(inputtedCourse);
-    });
-    
 
     calculateSemesterGPA();
-  })
+
+  });
+
+  // Reset entry fields
+  $("#add-semester").click(function(){
+
+    $("input#semester-name").val("");
+    $("#gpaForm")[0].reset();
+
+  });
+  $("#add-course").click(function(){
+    $("#course-grades").append(
+      '<br>'+
+      '<div class="course" class="form-group">'+
+        '<div class="row">'+
+          '<div class="col-md-5">'+
+            '<input type="text" placeholder="Course Name" id="course-name" class="form-control">'+
+          '</div>'+
+         ' <div class="col-md-5">'+
+            '<input type="text" placeholder="Course Credits" id="course-credits" class="form-control">'+
+          '</div>'+
+         ' <div class="col-md-2">'+
+            '<select name="letter-grade" id="letter-grade" class="form-select">'+
+              '<option value="">Letter Grade</option>'+
+              '<option value="4.0">A+</option>'+
+              '<option value="4.0">A</option>'+
+              '<option value="3.7">A-</option>'+
+              '<option value="3.3">B+</option>'+
+              '<option value="3.0">B</option>'+
+              '<option value="2.7">B-</option>'+
+              '<option value="2.3">C+</option>'+
+              '<option value="2.0">C</option>'+
+              '<option value="1.7">C-</option>'+
+              '<option value="1.3">D+</option>'+
+              '<option value="1.0">D</option>'+
+              '<option value="0.0">F</option>'+
+            '</select>'+
+          '</div>'+
+        '</div>'+
+      '</div>'
+    );
+  });
+
+  $("#calculate-gpa").click(function(){
+
+    event.preventDefault();
+    
+    $(".new-semester").each(function(){
+      
+      $(".course").each(function(){
+        var inputtedCourseName = $(this).find("input#course-name").val();
+        var inputtedCourseCredit = parseFloat($(this).find("input#course-credits").val());
+        var selectedGradePoint = parseFloat($(this).find("select option:selected").val());
+        var inputtedCourse = new Course(inputtedCourseName,inputtedCourseCredit,selectedGradePoint);
+      
+        courses.push(inputtedCourse);
+  
+      });
+  
+      calculateSemesterGPA();
+
+    });
+
+    calculateCumultativeGPA();
+
+  });
 });
