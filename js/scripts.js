06358352aa04f03e16b417367f4cff2883ff4118
Soila Pertet <nicolepertet@gmail.com>
2022-01-21 08:05:58 +0300

    Final commit

--- js/scripts.js
+++ js/scripts.js
@@ -122,7 +122,11 @@ $(document).ready(function(){
       calculateCumulativeGPA();
 
     });
-
+    var gpaLog = $('#gpa-history').offset().top;
+    $("HTML,BODY").animate({scrollTop:gpaLog},400,function(){
+   
+      window.location.summaryPosition = gpaLog;
+    });
   });
 
   // Reset entry fields
@@ -131,6 +135,12 @@ $(document).ready(function(){
     $("#gpa-history").hide();
     $("input#semester-name").val("");
     $("#gpaForm")[0].reset();
+
+    var gpaPosition = $('#gpaForm').offset().top;
+    $("HTML,BODY").animate({scrollTop:gpaPosition},400,function(){
+   
+      window.location.summaryPosition = gpaPosition;
+    });
 
   });
 
@@ -163,7 +173,7 @@ $(document).ready(function(){
   });
  
 
-  $("#save").click(function (e) {
+  $("#myForm").submit(function (e) {
     e.preventDefault();
     var name = $('#name').val();
     var phone = $('#phone').val();
@@ -198,82 +208,50 @@ $(document).ready(function(){
 
   });
 
+  $(".register").click(function(e) {
     e.preventDefault();
-    var name = $('#name').val();
-    var phone = $('#phone').val();
-    var level = $('#level').val();
-    var institute = $('#institute').val();
-    var experience = $('#experience').val();
-    var residence = $('#residence').val();
-    if (name == "") {
-      alert("please Enter your name");
-    } 
-    else if (phone == "") {
-      alert("Please input your phone number");
-    } 
-    else if (level == "") {
-      alert("Please select your Level");
-    } 
-    else if (institute == "") {
-      alert("Please enter your institute"); 
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var pass = $("#pass").val();
+    var cpass = $("#cpass").val();
+    if(name == "") {
+      alert("Please enter a Username")
+    }else if (email == "") {
+      alert("Please enter email")
+    }else if (pass == "") {
+      alert("Please enter pass")
+    }else if (cpass == "") {
+      alert("PConfirm Password")
+    }else{
+      alert("Registered Successfully")
+      window.location = "profile.html"; 
     }
-    else if (residence == "") {
-      console.log("Please enter your residence"); 
+    });
+
+  $("#signin").click(function(e) {
+    e.preventDefault();
+
+    var name = $(".name").val();
+    var pas = $("#pas").val();
+    if(name == ""){
+      alert("Please enter your Username")
+    }else if (pas == ""){
+      alert("Enter your Password")
+    }else{
+      alert("Login Successful")
+      window.location = "profile.html"; 
     }
-    else {
-      return true;
-    };
+  });
 
-    $("#upName").html("Your Name:   " + name);
-    $("#upPhone").html("Your Phone: " + phone);
-    $("#uplevel").html("Study Level: " + level);
-    $("#inst").html(" Institutution:" + institute);
-    // $("#red").html("reside");
-    $("#exp").html(" Experience:" + experience);
-    document.getElementById("myForm").reset();
-    $(".pic").hide();
-    $("#tit").show();
+  $(".help").click(function(e){
+    e.preventDefault();
 
-    $(".register").click(function(e) {
-      e.preventDefault();
-
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var pass = $("#pass").val();
-      var cpass = $("#cpass").val();
-      if (name == "") {
-        alert("Please enter a Username")
-      }else if (email == "") {
-        alert("Please enter email")
-      }else if (pass == "") {
-        alert("Please enter pass")
-      }else if (cpass == "") {
-        alert("PConfirm Password")
-      }else{
-        alert("Registered Successfully")
-        window.location = "profile.html"; 
-      }
-    });
-
-    $("#signin").click(function(e) {
-      var name = $(".name").val();
-      var pas = $("#pas").val();
-      if(name == ""){
-        alert("Please enter your Username")
-      }else if (pas == ""){
-        alert("Enter your Password")
-      }else{
-        alert("Login Successful")
-        window.location = "profile.html"; 
-      }
-    });
-
-    $(".help").click(function(e){
-      $(".sign").show();
-      $(".login").hide();
-      $(".h-tag").hide();
-      $(".help").hide();
-    });
+    $(".sign").show();
+    $(".login").hide();
+    $(".h-tag").hide();
+    $(".help").hide();
+  });
     
 });
 
